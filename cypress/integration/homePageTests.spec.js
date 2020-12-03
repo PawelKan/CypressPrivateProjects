a456a95de23f3f285f19c108d0da720e7643e425
Pawel Kania <grylion02@gmail.com>
2020-12-03 16:10:19 +0100

    HomePageTests-Add first test

--- cypress/integration/homePageTests.spec.js
+++ cypress/integration/homePageTests.spec.js
@@ -2,11 +2,19 @@
 
 const { homePage } = require("../support/PageObjects/HomePage/HomePageObject.spec");
 
+const urls = require('../fixtures/url.json')
+
 describe("HomePage tests", () => {
     it("Visit Home Page", () =>{
-        cy.visit("/");
+        cy.visit(urls.urlToHomePage);
+
+        cy.log("**1 Verify Woman Category labe**")
         homePage.getBtn_categoryWomen().should('be.visible').and('contain', "Women")
+
+        cy.log("**2 Verify Dresses Category labe**")
         homePage.getBtn_categoryDresses().should('be.visible').and('contain', "Dresses")
+
+        cy.log("**2 Verify T-shirts Category labe**")
         homePage.getBtn_categoryTshirts().should('be.visible').and('contain', "T-shirts")
     })
 })
