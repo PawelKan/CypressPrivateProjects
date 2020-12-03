e8615f37ae1f19ab4822878e15b1de4b868e53fe
Pawel Kania <grylion02@gmail.com>
2020-12-03 17:08:55 +0100

    HomePageTests

--- cypress/integration/homePageTests.spec.js
+++ cypress/integration/homePageTests.spec.js
@@ -4,9 +4,31 @@ const { homePage } = require("../support/PageObjects/HomePage/HomePageObject.spe
 
 describe("HomePage tests", () => {
     it("Visit Home Page", () =>{
+<<<<<<< Updated upstream
         cy.visit("/");
         homePage.getBtn_categoryWomen().should('be.visible').and('contain', "Women")
         homePage.getBtn_categoryDresses().should('be.visible').and('contain', "Dresses")
+=======
+        cy.visit(url.urlHomePage);
+
+        cy.log("**Verify Contact us button**")
+        homePage.getBtn_contactUs().should("be.visible").should('contain', "Contact us")
+
+        cy.log("**Verify Sign in button**")
+        homePage.getBtn_signIn().should("be.visible").should('contain', "Sign in")
+
+        cy.log("**Verify Women in Category list**")
+        homePage.getBtn_categoryWomen().should('be.visible').and('contain', "Women")
+
+        cy.log("**Verify Dresses in Category list**")
+        homePage.getBtn_categoryDresses().should('be.visible').and('contain', "Dresses")
+
+        cy.log("**Verify T-shirts in Category list**")
+>>>>>>> Stashed changes
         homePage.getBtn_categoryTshirts().should('be.visible').and('contain', "T-shirts")
+
+        homePage.getBtn_signIn().click()
+        cy.url().should("contain", "controller=authentication&back=my-account")
     })
+   
 })
--- cypress/support/PageObjects/HomePage/HomePageObject.spec.js
+++ cypress/support/PageObjects/HomePage/HomePageObject.spec.js
@@ -2,10 +2,11 @@
 
 class HomePage {
 
-    getBtn_contactUs() { return cy.get('#contact-link > a') }
+    getBtn_contactUs() { return cy.get('#contact-link > a[title="Contact Us"]') }
     getBtn_signIn() { return cy.get('.login') }
     getBtn_categoryWomen() { return cy.get('#block_top_menu > ul >li').first() }
     getBtn_categoryDresses() { return cy.get('.sf-menu > :nth-child(2) > .sf-with-ul') }
     getBtn_categoryTshirts() { return cy.get('.sf-menu > :nth-child(3) > a') }
+   
 }
 export const homePage = new HomePage()
